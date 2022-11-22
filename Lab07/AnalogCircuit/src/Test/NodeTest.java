74e30ecbd9948f83b140999061c8b5e12eb90569
Jackie Li <li.jackie565@gmail.com>
2022-11-21 23:03:11 -0500

    Lab07 Finished

---
+++ Lab07/AnalogCircuit/src/Test/NodeTest.java
@@ -0,0 +1,19 @@
+package Test;
+import coe318.lab7.*;
+import org.junit.Test;
+import org.junit.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class NodeTest {
+    static Node n1 = new Node();
+    static Node n2 = new Node();
+    static Node n3 = new Node();
+
+    @Test
+    public void TestNode() { 
+        assertEquals("Passed",0, n1.nodeID);
+        assertEquals("Passed",1, n2.nodeID);
+        assertEquals("Passed",2, n3.nodeID);
+    } 
+}
